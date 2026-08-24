1079ab805b124705b79c7479c283782b7f5e38e9
Aleks Kotik <alex.kotik.net@gmail.com>
2026-08-24 21:44:54 +0200

    refactor: plan analisys

--- app/src/main/java/com/smartbox/investory/config/AiAnalysisSchedulingConfig.java
+++
@@ -1,9 +0,0 @@
-package com.smartbox.investory.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-/** Enables cron-based AI portfolio reports. */
-@Configuration
-@EnableScheduling
-public class AiAnalysisSchedulingConfig {}
